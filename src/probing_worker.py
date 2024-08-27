15a94698cf1de3a5b4208add874da59649383ea1
holmesbenchmark <holmesbenchmark@gmail.com>
2024-08-27 21:01:13 +0200

    updated reqs

--- src/probing_worker.py
+++ src/probing_worker.py
@@ -108,7 +108,7 @@ class GeneralProbeWorker(ProbeWorker):
             callbacks=[ModelCheckpoint(monitor="val loss",  mode="min", dirpath=log_dir), EarlyStopping(monitor="val loss",  mode="min", patience=5)]
         )
 
-        trainer.fit(model=probing_model, train_dataloader=train_dataloader, val_dataloaders=[dev_dataloader])
+        trainer.fit(model=probing_model, train_dataloaders=[train_dataloader], val_dataloaders=[dev_dataloader])
 
         trainer.test(ckpt_path="best", test_dataloaders=[test_dataloader])
 

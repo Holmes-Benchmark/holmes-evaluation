7ca655f58dfc8a626cb2b636b3c1b6535e02f0a1
holmesbenchmark <holmesbenchmark@gmail.com>
2024-10-14 19:01:08 +0200

    updated download paths

--- data/flash-holmes/download.py
+++ data/flash-holmes/download.py
@@ -4,7 +4,7 @@ from os.path import exists
 
 current_directory = os.path.split(os.getcwd())[1]
 
-url = "https://drive.google.com/file/d/1sStwl0dFrg7GnyQ4SqBOTbfH93EArnqJ/view?usp=sharing"
+url = "https://drive.google.com/file/d/1VK3xR_ZMQUBIRgY8JElHlqtxGsgLTWm3/view?usp=sharing"
 
 output_file = "flash-holmes.zip"
 
